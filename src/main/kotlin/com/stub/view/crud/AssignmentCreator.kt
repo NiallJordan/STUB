cec43f40827d4f39e751e4afc9a208980a9879cc
NiallJordan <niall.jordan2012@gmail.com>
2020-10-29 22:23:51 +0000

    Layout rearranged, delete implented fully

--- src/main/kotlin/com/stub/view/crud/AssignmentCreator.kt
+++ src/main/kotlin/com/stub/view/crud/AssignmentCreator.kt
@@ -1,46 +1,99 @@
-package com.stub.view.crud
+package com.stub.view.crud//package com.stub.view.crud
 
 import com.stub.controller.AssignmentController
 import com.stub.model.AssignModel
 import com.stub.model.Assignment
-import com.stub.view.MyView
 import tornadofx.*
 
 
 class AssignmentCreator: View("Assignment Creator"){
-    val assignment : AssignModel by inject()
+    val model : AssignModel by inject()
     val assignmentController : AssignmentController by inject()
 
-    override val headingProperty = assignment.title
+    override val headingProperty = model.title
 
     override val root = form {
         fieldset {
             field("Module Name") {
-                textfield(assignment.module)
+                textfield(model.module){
+                    this.required()
+//                    validator {
+//                        when {
+//                            it?.isEmpty()!! -> error("Module cannot be empty")
+//                            it!!.length >65 -> error("Too Long")
+//                            else -> null
+//                        }
+//                    }
+                }
             }
             field("Assignment Title") {
-                textfield(assignment.title)
+                textfield(model.title){
+                    this.required()
+//                    validator {
+//                        when {
+//                            it?.isEmpty()!! -> error("Title cannot be empty")
+//                            it!!.length >65 -> error("Too Long")
+//                            else -> null
+//                        }
+//                    }
+                }
             }
             field("Description") {
-                textarea(assignment.description)
+                textarea(model.description){
+                    this.required()
+//                    validator {
+//                        when {
+//                            it?.isEmpty()!! -> error("Description cannot be empty")
+//                            it!!.length >500 -> error("Description can not be longer than 500 characters")
+//                            else -> null
+//                        }
+//                    }
+                }
             }
 
             field("Percentage Weighting (%)") {
-                textfield(assignment.weight)
+                textfield(model.weight){
+                    this.required()
+                    validator {
+                        when(it) {
+                            null -> error("Weighting cannot be empty")
+                            else -> null
+                        }
+                    }
+                }
             }
 
             field("Submission Link") {
-                textfield(assignment.subLink)
+                textfield(model.subLink){
+                    this.required()
+//                    validator {
+//                        when {
+//                            it?.isEmpty()!! -> error("Submission Link cannot be empty")
+//                            it!!.length >65 -> error("Too Long")
+//                            else -> null
+//                        }
+//                    }
+                }
             }
             field("Date Due") {
-                textfield(assignment.subDate)
+                textfield(model.subDate){
+                    this.required()
+//                    validator {
+//                        when {
+//                            it?.isEmpty()!! -> error("Submission Date cannot be empty")
+//                            it!!.length >65 -> error("Too Long")
+//                            else -> null
+//                        }
+//                    }
+                }
             }
         }
     }
 
     override fun onCreate() {
-        val assignment = Assignment(assignment.module.value,assignment.title.value,assignment.description.value,assignment.weight.value,assignment.subLink.value,assignment.subDate.value)
+        val assignment = Assignment(model.module.value,model.title.value,model.description.value,model.weight.value,model.subLink.value,model.subDate.value)
         assignmentController.createNewAssignment(assignment)
+        ///workspace.undock<AssignmentCreator>
         workspace.dock<MyView>()
     }
 }
